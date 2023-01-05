1ee8179294fc7cf64432cf9581ec087f8bbd814d
Alois Sečkár <ellrohir@seznam.cz>
2023-01-05 20:57:35 +0100

    nuxt-tailwind demo project

---
+++ nuxt-tailwind/pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <div>
+            This demo shows how to include Tailwind in Nuxt 3 application
+        </div>
+        <div class="bg-gray-700 text-red-600">
+            div element customized with Tailwind inline classes
+        </div>
+        <div class="customized">
+            div element customized with Tailwind @apply
+        </div>
+    </div>
+</template>
