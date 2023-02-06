a61aed12b40b28fc2e454ad51679328471b5ba25
Alois Sečkár <ellrohir@seznam.cz>
2023-02-06 23:20:45 +0100

    nuxt-api project

---
+++ nuxt-api/pages/index.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <h1>Nuxt 3 - API</h1>
+        <button @click="handle">Make API call</button>
+        <div>{{ apiResponse }}</div>
+    </div>
+</template>
+
+<script setup lang="ts">
+const apiResponse = ref('')
+
+const handle = async () => {
+    const { data } = await useFetch('/api/sample', { method: 'GET'})
+    if (data) {
+        apiResponse.value = data.value
+    } else {
+        apiResponse.value = "ERROR"
+    }
+}
+</script>
