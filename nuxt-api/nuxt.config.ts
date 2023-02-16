e668e5482358350125be4557b9f8a1f2795b2443
Alois Sečkár <ellrohir@seznam.cz>
2023-02-16 22:15:05 +0100

    nuxt-api project update

--- nuxt-api/nuxt.config.ts
+++ nuxt-api/nuxt.config.ts
@@ -1,3 +1,3 @@
+// https://nuxt.com/docs/guide/directory-structure/nuxt.config
 export default defineNuxtConfig({
 })
-  
--- nuxt-api/server/api/error.get.ts
+++ nuxt-api/server/api/error.get.ts
@@ -1,14 +1,14 @@
 /**
- * 
+ *
  * http://localhost:3000/api/error/
- * 
+ *
  * This method demonstrates the behaviour of 'createError' method in Nuxt.
  * It just throws a error with random HTTP code 400-410.
- * 
+ *
  * When then error code is 404 (Not found), Nuxt does nothing special.
  * When the code is anything else but 404, then stack trace is appended.
  * However, this ONLY works when app runs in DEV mode, for apparent security reasons.
- * 
+ *
  */
 
 export default defineEventHandler(() => {
@@ -18,4 +18,3 @@ export default defineEventHandler(() => {
     message: code.toString()
   })
 })
-  
