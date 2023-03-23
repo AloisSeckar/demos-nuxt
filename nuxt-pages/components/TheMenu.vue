3301479beb9b3e40e7a2a1812bae8c8a9dcb1107
Alois Sečkár <ellrohir@seznam.cz>
2023-03-23 21:58:54 +0100

    nuxt-pages project

---
+++ nuxt-pages/components/TheMenu.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <hr>
+    <NuxtLink to="/">
+      Index
+    </NuxtLink>
+    &#x2022;
+    <NuxtLink to="/first">
+      First
+    </NuxtLink>
+    &#x2022;
+    <NuxtLink to="/second">
+      Second
+    </NuxtLink>
+    <hr>
+  </div>
+</template>
---
+++ nuxt-pages/pages/first.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <h2>This is the first sub-page</h2>
+    <div>
+      Page specific content (TO BE REPLACED WITH COMPONENT)
+    </div>
+    <TheMenu />
+  </div>
+</template>
