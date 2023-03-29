fd9cca614cf50b20b52f2d940cadb08fe16132d2
Alois Sečkár <ellrohir@seznam.cz>
2023-03-29 23:22:50 +0200

    nuxt-pinia project

---
+++ nuxt-pinia/app.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h1>Nuxt + Pinia</h1>
+    <div>Working starter for <a href="https://nuxt.com/" title="Nuxt">Nuxt</a> project</div>
+    <div>
+      Here you can see, how to integrate <a href="https://pinia.vuejs.org/" title="Pinia">Pinia</a> state management into your application.
+    </div>
+    <h2>Stored values</h2>
+    <div><strong>Boolean value:</strong> {{ store.demoBoolean }}</div>
+    <div><strong>Number value:</strong> {{ store.demoNumber }}</div>
+    <div><strong>String value:</strong> {{ store.demoString }}</div>
+    <div><strong>Array value:</strong> {{ store.demoArray }}</div>
+    <button @click="store.fillWithRandomValues()">Init</button>
+    <button @click="getterDemo">Getter</button>
+  </div>
+</template>
+
+<script setup lang="ts">
+const store = usePiniaDemoStore()
+const getterDemo = () => {
+  alert(store.getDouble())
+}
+</script>
