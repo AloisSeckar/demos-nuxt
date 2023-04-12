f1e270defcfc1b9a175765a60c3f4ffb415e92ad
Alois Sečkár <ellrohir@seznam.cz>
2023-04-12 22:13:48 +0200

    nuxt-content project

---
+++ nuxt-content/app.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h1>nuxt-content</h1>
+    <div>
+      The following page content is rendered with help of 
+      <a href="https://content.nuxtjs.org/" title="Nuxt">Nuxt Content</a> module:
+    </div>
+    <hr />
+    <div>
+      <ContentDoc />
+    </div>
+    <hr />
+    <div>
+      All you need to do to start benefit from this is to 
+      <a href="https://content.nuxtjs.org/get-started#add-to-a-project">include the module</a>, 
+      add <pre>&lt;ContentDoc /&gt;</pre> component into template
+      and provide corresponding sources into <strong>/content</strong> directory.
+    </div>
+  </div>
+</template>
