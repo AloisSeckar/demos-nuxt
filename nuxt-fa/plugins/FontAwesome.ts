ebbec0491f945a4faa7e09658193f118f9b93d75
Alois Sečkár <ellrohir@seznam.cz>
2023-06-18 21:08:42 +0200

    eslint error workaround

--- nuxt-fa/plugins/FontAwesome.ts
+++ nuxt-fa/plugins/FontAwesome.ts
@@ -1,6 +1,8 @@
 import { library } from '@fortawesome/fontawesome-svg-core'
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import { faEnvelope, faFaceSmile, faHouse } from '@fortawesome/free-solid-svg-icons'
+// because of https://github.com/FortAwesome/vue-fontawesome/issues/448
+// eslint-disable-next-line import/named
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
 library.add(faEnvelope)
 library.add(faFaceSmile)
