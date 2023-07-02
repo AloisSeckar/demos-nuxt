de518597e8817002f5180ed3b4b21a9b837bb0ba
Alois Sečkár <ellrohir@seznam.cz>
2023-07-02 20:39:51 +0200

    clean-up

--- nuxt-pinia/app.vue
+++ nuxt-pinia/app.vue
@@ -1,7 +1,6 @@
 <template>
   <div>
     <h1>Nuxt + Pinia</h1>
-    <div>Working starter for <a href="https://nuxt.com/" title="Nuxt">Nuxt</a> project</div>
     <div>
       Here you can see, how to integrate <a href="https://pinia.vuejs.org/" title="Pinia">Pinia</a> state management into your application.
     </div>
@@ -10,8 +9,12 @@
     <div><strong>Number value:</strong> {{ store.demoNumber }}</div>
     <div><strong>String value:</strong> {{ store.demoString }}</div>
     <div><strong>Array value:</strong> {{ store.demoArray }}</div>
-    <button @click="store.fillWithRandomValues()">Init</button>
-    <button @click="getterDemo">Getter</button>
+    <button @click="store.fillWithRandomValues()">
+      Init
+    </button>
+    <button @click="getterDemo">
+      Getter
+    </button>
   </div>
 </template>
 
