743b02813a4a31a0085edcfd24adc6c0f1c6dddf
Alois Sečkár <ellrohir@seznam.cz>
2023-09-11 23:43:45 +0200

    link styling

--- nuxt-stack/app.vue
+++ nuxt-stack/app.vue
@@ -4,14 +4,18 @@
     <div>{{ $t("subtitle") }}</div>
     <NuxtPage />
     <p>
-      Current time by <NuxtLink to="https://vueuse.org/">
-        VueUse:
-      </NuxtLink>
+      Current time by
+      <span class="link">
+        <NuxtLink to="https://vueuse.org/">VueUse</NuxtLink>
+      </span>
+      :
       {{ useDateFormat(useNow(), 'HH:mm:ss DD.MM.YYYY').value.replace('"', '') }}
     </p>
-    <NuxtLink to="https://github.com/AloisSeckar/demos-nuxt/tree/master/nuxt-stack" class="text-xs hover:text-amber-200">
-      https://github.com/AloisSeckar/demos-nuxt/tree/master/nuxt-stack
-    </NuxtLink>
+    <div class="link text-xs">
+      <NuxtLink to="https://github.com/AloisSeckar/demos-nuxt/tree/master/nuxt-stack">
+        https://github.com/AloisSeckar/demos-nuxt/tree/master/nuxt-stack
+      </NuxtLink>
+    </div>
   </div>
 </template>
 
