d81749fdde434f7d7a0174f29fc800bc278e82d4
Alois Sečkár <ellrohir@seznam.cz>
2023-09-27 14:43:09 +0200

    chore: minor linting and TS fixes

--- nuxt-content/app.vue
+++ nuxt-content/app.vue
@@ -2,17 +2,17 @@
   <div>
     <h1>nuxt-content</h1>
     <div>
-      The following page content is rendered with help of 
+      The following page content is rendered with help of
       <a href="https://content.nuxtjs.org/" title="Nuxt">Nuxt Content</a> module:
     </div>
-    <hr />
+    <hr>
     <div>
       <ContentDoc />
     </div>
-    <hr />
+    <hr>
     <div>
-      All you need to do to start benefit from this is to 
-      <a href="https://content.nuxtjs.org/get-started#add-to-a-project">include the module</a>, 
+      All you need to do to start benefit from this is to
+      <a href="https://content.nuxtjs.org/get-started#add-to-a-project">include the module</a>,
       add <pre>&lt;ContentDoc /&gt;</pre> component into template
       and provide corresponding sources into <strong>/content</strong> directory.
     </div>
--- nuxt-pinia/composables/usePiniaDemoStore.ts
+++ nuxt-pinia/composables/usePiniaDemoStore.ts
@@ -1,26 +1,25 @@
 export const usePiniaDemoStore = defineStore({
-    id: 'pinia-demo',
-    state: () => {
-      return {
-        demoBoolean: false,
-        demoNumber: -1,
-        demoString: '',
-        demoArray: []
-      }
-    },
-    getters: {
-        getDouble: (state) => {
-            return () => state.demoNumber * 2
-        }
-    },
-    actions: {
-      fillWithRandomValues (): void {
-        this.demoBoolean = true
-        this.demoNumber = Math.floor(Math.random() * 100);
-        this.demoString = this.demoNumber.toString()
-        this.demoArray = []
-        this.demoArray.push(this.demoBoolean, this.demoNumber, this.demoString)
-      }
+  id: 'pinia-demo',
+  state: () => {
+    return {
+      demoBoolean: false,
+      demoNumber: -1,
+      demoString: '',
+      demoArray: [] as any[]
     }
-  })
-  
+  },
+  getters: {
+    getDouble: (state) => {
+      return () => state.demoNumber * 2
+    }
+  },
+  actions: {
+    fillWithRandomValues (): void {
+      this.demoBoolean = true
+      this.demoNumber = Math.floor(Math.random() * 100)
+      this.demoString = this.demoNumber.toString()
+      this.demoArray = []
+      this.demoArray.push(this.demoBoolean, this.demoNumber, this.demoString)
+    }
+  }
+})
