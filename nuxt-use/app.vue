656e383fa85d71c675c7432d3c88210b0a94fa0c
Alois Sečkár <ellrohir@seznam.cz>
2023-10-17 08:56:29 +0200

    feat: nuxt-use project stub

---
+++ nuxt-use/app.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <h1>nuxt-use</h1>
+    <div>Various demos of <a href="https://vueuse.org/" title="VueUse">VueUse</a> features in Nuxt</div>
+    <div>
+      TODO
+    </div>
+  </div>
+</template>
