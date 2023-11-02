472631b552746e038525071f7d43420576baea16
Alois Sečkár <ellrohir@seznam.cz>
2023-11-02 19:49:22 +0100

    chore: Nuxt 3.8 type imports

--- nuxt-formkit/formkit.config.ts
+++ nuxt-formkit/formkit.config.ts
@@ -1,6 +1,6 @@
 // https://formkit.com/getting-started/installation#configuring-nuxt
 import { en, de, cs } from '@formkit/i18n'
-import { DefaultConfigOptions } from '@formkit/vue'
+import type { DefaultConfigOptions } from '@formkit/vue'
 
 const config: DefaultConfigOptions = {
   // example of importing different message translations and set the default one
