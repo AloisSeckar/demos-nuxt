33fa6a39355ea83a964d8096e57224b3442f6b55
Alois Sečkár <ellrohir@seznam.cz>
2023-11-17 18:32:57 +0100

    feat: useMouse demo

---
+++ nuxt-use/pages/mouse.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <p>
+      <pre style="display: inline-block;">useMouse</pre> returns reactive coordinates of current mouse position:
+      <pre style="display: inline-block;">{{ x }}:{{ y }}</pre>
+      <br>Here it is used in conjunction with `v-bind` to CSS `top` and `left` attributes to create an element that always follows the cursor.
+    </p><div class="mouse-follower" />
+  </div>
+</template>
+
+<script setup>
+const { x, y } = useMouse()
+</script>
+
+  <style scoped>
+  .mouse-follower {
+    position: absolute;
+    width: 50px;
+    height: 50px;
+    background-color: red;
+    top: v-bind('y + "px"');
+    left: v-bind('x + "px"');
+  }
+  </style>
