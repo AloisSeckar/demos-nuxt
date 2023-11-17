cbd7f3e848b47994ca73ec8dc66df66bc43391d6
Alois Sečkár <ellrohir@seznam.cz>
2023-11-17 18:39:12 +0100

    feat: nuxt-use layout adjustments

--- nuxt-use/pages/index.vue
+++ nuxt-use/pages/index.vue
@@ -1,14 +1,17 @@
 <template>
-  <ul>
-    <li>
-      <NuxtLink to="/mouse">
-        useMouse
-      </NuxtLink>
-    </li>
-    <li>
-      <NuxtLink to="/scroll">
-        useInfiniteScroll
-      </NuxtLink>
-    </li>
-  </ul>
+  <div>
+    <div>Various demos of <a href="https://vueuse.org/" title="VueUse">VueUse</a> features in Nuxt</div>
+    <ul>
+      <li>
+        <NuxtLink to="/mouse">
+          useMouse
+        </NuxtLink>
+      </li>
+      <li>
+        <NuxtLink to="/scroll">
+          useInfiniteScroll
+        </NuxtLink>
+      </li>
+    </ul>
+  </div>
 </template>
