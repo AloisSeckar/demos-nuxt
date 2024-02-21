00bf72c193bfd23fd2ad0beced97368738bcd626
Alois Sečkár <ellrohir@seznam.cz>
2024-02-21 21:01:15 +0100

    feat: nuxt-layouts project

---
+++ nuxt-layouts/layouts/default.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <h1>default layout</h1>
+        <div>This is the default layout used for a page if nothing else is set</div>
+        <div>The page content will be rendered between the lines below</div>
+        <a href="/">To page 1</a>&nbsp;
+        <a href="/second">To page 2</a>&nbsp;
+        <a href="/third">To page 3</a>
+        <hr />
+        <slot />
+        <hr />     
+        <div>Footer (defined in default layout)</div>   
+    </div>
+</template>
---
+++ nuxt-layouts/pages/third.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <div>
+      This is the third page, where you can toggle between default and special layout
+    </div>
+    <div>
+      <button @click="toggleLayout">
+        Update layout
+      </button>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+definePageMeta({
+  layout: 'default'
+})
+
+function toggleLayout () {
+  // get current layout name via `useRoute` composable
+  const layout = useRoute().meta.layout === 'default' ? 'special' : 'default'
+  // built-in function for dynamic layout changes
+  setPageLayout(layout)
+}
+</script>
