58dd5e5ce2b449274f14b322a17428f29e6d1f3e
Alois Sečkár <ellrohir@seznam.cz>
2024-02-21 21:21:56 +0100

    refactor: get rid of layout in nuxt-dx

--- nuxt-dx/app.vue
+++ nuxt-dx/app.vue
@@ -1,12 +1,24 @@
 <template>
     <div>
-        <!-- /layout/default.vue renders here by default -->
-        <NuxtLayout />
+        <h1>Nuxt + DevExtreme</h1>
+        <DxButton 
+            text="I am the DevExtreme button"
+            @click="handle">
+        </DxButton>
+        <div>{{ clickResponse }}</div>
     </div>
 </template>
 
 <script setup lang="ts">
 // simple import of predefined DevExtreme CSS theme
 // https://js.devexpress.com/Documentation/Guide/Themes_and_Styles/Predefined_Themes/
-import 'devextreme/dist/css/dx.light.css';
+import 'devextreme/dist/css/dx.light.css'
+
+const clickResponse = ref('')
+const clicks = ref(0)
+
+const handle = () => {
+    clicks.value = clicks.value + 1
+    clickResponse.value = `Button was clicked ${clicks.value} times`
+}
 </script>
