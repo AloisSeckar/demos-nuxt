d1d919f10e8110cd30dde013ac9ce9bb6ce61406
Alois Sečkár <ellrohir@seznam.cz>
2024-02-21 21:29:35 +0100

    docs: info about layouts

--- nuxt-layouts/app.vue
+++ nuxt-layouts/app.vue
@@ -1,3 +1,14 @@
+<!--
+    https://nuxt.com/docs/guide/directory-structure/layouts
+
+    layouts are useful to declare common parts of an appliction only
+    once and in a dedicated place where everyone familiar with Nuxt
+    will look for them
+
+    NOTE: only use layouts if you have MORE THAN ONE possible page layouts in your application
+    if there is only one, it is recommended by Nuxt team to put the common parts into app.vue
+-->
+
 <template>
   <NuxtLayout>
     <NuxtPage />
