90f34dbb7fbcfbd66b6823269f12c816fcd89055
Alois Sečkár <ellrohir@seznam.cz>
2024-03-25 20:41:45 +0100

    chore: update scripts to reduce outputs

--- eslint-check.bat
+++ eslint-check.bat
@@ -1,9 +1,10 @@
 @echo off
-
 FOR /F %%G IN (.\project-list.txt) DO (
-  if not "%%G"=="nuxt-minimal" (
+  @echo off
+  if not "%%G"=="..\nuxt-minimal" (
     echo tech-update %%G
     cd %%G
     pnpm eslint
   )
+  @echo off
 )
--- tech-update.bat
+++ tech-update.bat
@@ -1,8 +1,9 @@
 @echo off
-
 FOR /F %%G IN (.\project-list.txt) DO (
+  @echo off
   echo tech-update %%G
   cd %%G
   pnpm install
   pnpm audit fix
+  @echo off
 )
