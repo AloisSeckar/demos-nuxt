ee7126a4b5af16e42e7b937f3cea50e128480239
Alois Sečkár <ellrohir@seznam.cz>
2024-03-25 19:47:33 +0100

    fix: deal with eslint feedback

--- nuxt-dx/app.vue
+++ nuxt-dx/app.vue
@@ -1,12 +1,12 @@
 <template>
-    <div>
-        <h1>Nuxt + DevExtreme</h1>
-        <DxButton 
-            text="I am the DevExtreme button"
-            @click="handle">
-        </DxButton>
-        <div>{{ clickResponse }}</div>
-    </div>
+  <div>
+    <h1>Nuxt + DevExtreme</h1>
+    <DxButton
+      text="I am the DevExtreme button"
+      @click="handle"
+    />
+    <div>{{ clickResponse }}</div>
+  </div>
 </template>
 
 <script setup lang="ts">
@@ -18,7 +18,7 @@ const clickResponse = ref('')
 const clicks = ref(0)
 
 const handle = () => {
-    clicks.value = clicks.value + 1
-    clickResponse.value = `Button was clicked ${clicks.value} times`
+  clicks.value = clicks.value + 1
+  clickResponse.value = `Button was clicked ${clicks.value} times`
 }
 </script>
