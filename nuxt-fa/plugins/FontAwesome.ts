79b1510eb5409c842d99118bf9513cc05a8ca34b
Alois Sečkár <ellrohir@seznam.cz>
2024-03-25 19:47:33 +0100

    fix: deal with eslint feedback

--- nuxt-fa/plugins/FontAwesome.ts
+++ nuxt-fa/plugins/FontAwesome.ts
@@ -1,7 +1,5 @@
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { faEnvelope, faFaceSmile, faHouse } from '@fortawesome/free-solid-svg-icons'
-// because of https://github.com/FortAwesome/vue-fontawesome/issues/448
-// eslint-disable-next-line import/named
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
 library.add(faEnvelope)
