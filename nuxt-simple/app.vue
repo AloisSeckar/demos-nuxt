fdc8595978aba5095e79624807e4ac927c902c15
Alois Sečkár <ellrohir@seznam.cz>
2024-05-04 22:05:41 +0200

    refactor: sync nuxt-simple with nuxt-starter variant

--- nuxt-simple/app.vue
+++ nuxt-simple/app.vue
@@ -1,11 +1,15 @@
 <template>
   <div>
     <h1>nuxt-simple</h1>
-    <div>Working starter for <a href="https://nuxt.com/">Nuxt</a> project</div>
+    <div>
+      Working starter for <a href="https://nuxt.com/">Nuxt</a> project
+    </div>
     <div>
       See <a href="https://github.com/AloisSeckar/demos-nuxt">project home</a>
       for more details and my Nuxt 3 tutorial and examples.
     </div>
-    <div>Start editing <strong>app.vue</strong> file to get started with your Nuxt application</div>
+    <div>
+      Start editing <strong>app.vue</strong> file to get started with your Nuxt application
+    </div>
   </div>
 </template>
