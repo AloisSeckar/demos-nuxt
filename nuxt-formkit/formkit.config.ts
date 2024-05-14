3b7874f394327cb4a59c9bed212ff9951fbabf8b
Alois Sečkár <ellrohir@seznam.cz>
2024-05-14 22:02:33 +0200

    fix: hydration mismatch due to icons

--- nuxt-formkit/formkit.config.ts
+++ nuxt-formkit/formkit.config.ts
@@ -1,13 +1,15 @@
 // https://formkit.com/getting-started/installation#configuring-nuxt
+import '@formkit/themes/genesis'
 import { en, de, cs } from '@formkit/i18n'
 import type { DefaultConfigOptions } from '@formkit/vue'
+import { genesisIcons } from '@formkit/icons'
 
 const config: DefaultConfigOptions = {
   // example of importing different message translations and set the default one
   locales: { en, de, cs },
   locale: 'en',
-  // use default css styling https://formkit.com/essentials/styling
-  theme: 'genesis',
+  // to fix SSR issue - https://github.com/formkit/formkit/issues/539
+  icons: { ...genesisIcons },
 }
 
 export default config
