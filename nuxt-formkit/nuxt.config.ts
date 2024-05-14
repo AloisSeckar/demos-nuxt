48f6b41f88f1d28a59a60ac2529bd744dd34e1ac
Alois Sečkár <ellrohir@seznam.cz>
2024-05-14 22:02:33 +0200

    fix: hydration mismatch due to icons

--- nuxt-formkit/nuxt.config.ts
+++ nuxt-formkit/nuxt.config.ts
@@ -1,7 +1,7 @@
 // Nuxt project configuration like modules or runtime variables goes here
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  ssr: false, // TODO solve hydration mismatch in SSR
+  css: ['@formkit/themes/genesis'],
   modules: [
     '@formkit/nuxt',
     '@nuxt/eslint',
