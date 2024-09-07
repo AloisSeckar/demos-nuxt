8e0c00f2086de707ae49725b82deffcb64f09627
Alois Sečkár <ellrohir@seznam.cz>
2024-09-07 23:28:46 +0200

    chore: maintenance scripts update

--- tech-update.bat
+++ tech-update.bat
@@ -3,6 +3,8 @@ FOR /F %%G IN (.\project-list.txt) DO (
   @echo off
   echo tech-update %%G
   cd %%G
+  rmdir /S /Q node_modules
+  del /Q pnpm-lock.yaml
   pnpm install
   pnpm audit --production fix
   @echo off
