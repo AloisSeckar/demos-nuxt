5472fa7dd1f4c9d6fd21182c7b4e42ba70cb9774
Alois Sečkár <ellrohir@seznam.cz>
2024-09-07 22:39:40 +0200

    build: tech update 09/2024

--- nuxt-fa/nuxt.config.ts
+++ nuxt-fa/nuxt.config.ts
@@ -1,7 +1,7 @@
 // Nuxt project configuration like modules or runtime variables goes here
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-07-04',
+  compatibilityDate: '2024-09-07',
   ssr: false, // fa component doesn't work correctly with SSR
   modules: [
     '@nuxt/eslint',
--- nuxt-pinia/nuxt.config.ts
+++ nuxt-pinia/nuxt.config.ts
@@ -1,7 +1,7 @@
 // Nuxt project configuration like modules or runtime variables goes here
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-07-04',
+  compatibilityDate: '2024-09-07',
   modules: [
     '@nuxt/eslint',
     '@pinia/nuxt',
