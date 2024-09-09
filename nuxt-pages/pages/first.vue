c117c127dc99e82dcc12544be51962332fdb9d7f
Alois Sečkár <ellrohir@seznam.cz>
2024-09-09 20:46:47 +0200

    fix: correct mis-leading text

--- nuxt-pages/pages/first.vue
+++ nuxt-pages/pages/first.vue
@@ -2,7 +2,7 @@
   <div>
     <h2>This is the first sub-page</h2>
     <div>
-      <TextBox text-to-display="This is the content of FIRST sub-page defined as component prop" />
+      <TextBox text-to-display="This is the content of FIRST sub-page" />
     </div>
   </div>
 </template>
--- nuxt-pages/pages/index.vue
+++ nuxt-pages/pages/index.vue
@@ -2,7 +2,7 @@
   <div>
     <h2>This is the index page</h2>
     <div>
-      <TextBox text-to-display="This is the content of INDEX page defined as component prop" />
+      <TextBox text-to-display="This is the content of INDEX page" />
     </div>
   </div>
 </template>
