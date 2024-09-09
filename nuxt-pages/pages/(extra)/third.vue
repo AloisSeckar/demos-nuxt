1d93be19dee6707921415fa63af5ed4d55c6cb69
Alois Sečkár <ellrohir@seznam.cz>
2024-09-09 20:46:36 +0200

    feat: Nuxt 3.13 route groups example

---
+++ nuxt-pages/pages/(extra)/third.vue
@@ -0,0 +1,8 @@
+<template>
+  <div>
+    <h2>This is the third sub-page</h2>
+    <div>
+      <TextBox text-to-display="Since Nuxt 3.13, folder inside (brackets) is ignored and not included in the route. This feature is called `Route groups`" />
+    </div>
+  </div>
+</template>
