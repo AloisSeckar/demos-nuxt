97a3d28e52eef7684cdf41164e40bbf94f828de2
Alois Sečkár <ellrohir@seznam.cz>
2024-09-09 20:46:47 +0200

    fix: correct mis-leading text

--- nuxt-pages/pages/second.vue
+++ nuxt-pages/pages/second.vue
@@ -2,7 +2,7 @@
   <div>
     <h2>This is the second sub-page</h2>
     <div>
-      <TextBox text-to-display="This is the content of SECOND sub-page defined as component prop" />
+      <TextBox text-to-display="This is the content of SECOND sub-page" />
     </div>
   </div>
 </template>
