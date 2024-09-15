7830bf3212b96e0d24f7e635da8ecb9c5e69560f
Alois Sečkár <ellrohir@seznam.cz>
2024-09-15 15:05:51 +0200

    fix: display submission as date-time

--- nuxt-formkit/pages/index.vue
+++ nuxt-formkit/pages/index.vue
@@ -17,7 +17,7 @@ const formState = ref('form was not submitted')
 
 // handler listening to form submissions
 function handleSave(formData: Bio) {
-  formState.value = 'form was submitted @ ' + new Date().toDateString()
+  formState.value = 'form was submitted @ ' + new Date().toISOString()
   // just test output
   // in real-world scenario you'd probably call some backend function to persist the data
   console.log(formState.value)
