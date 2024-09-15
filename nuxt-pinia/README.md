a36bfcef2d65ad85938e9415252365fe16a68cdb
Alois Sečkár <ellrohir@seznam.cz>
2024-09-15 14:46:34 +0200

    feat: Pinia OptionsAPI and CompositionAPI style

--- nuxt-pinia/README.md
+++ nuxt-pinia/README.md
@@ -8,5 +8,7 @@ This demo shows how to include [Pinia](https://pinia.vuejs.org/) in [Nuxt](https
 4. `pnpm dev` 
 5. `localhost:3000` 
 
+The app show simple implementation of Pinia data stores in both possible API styles - OptionsAPI and CompositionAPI. Both are functionally equiavalent and you can pick the style that suits you better.
+
 ### Tutorial article
 https://master-coda.cz/article/nuxt-pinia
---
+++ nuxt-pinia/components/DemoStoreOptions.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="wrapper">
+    <h2>usePiniaOptionsStore()</h2>
+    <div>
+      Pinia store written in <b>OptionsAPI</b> style is used
+    </div>
+    <h3>Stored values</h3>
+    <div><strong>Boolean value:</strong> {{ store.demoBoolean }}</div>
+    <div><strong>Number value:</strong> {{ store.demoNumber }}</div>
+    <div><strong>String value:</strong> {{ store.demoString }}</div>
+    <div><strong>Array value:</strong> {{ store.demoArray }}</div>
+    <br>
+    <button @click="store.fillWithRandomValues()">
+      Init
+    </button>
+    &nbsp;
+    <button @click="getterDemo">
+      Getter
+    </button>
+  </div>
+</template>
+
+<script setup lang="ts">
+const store = usePiniaOptionsStore()
+const getterDemo = () => {
+  alert(store.getDouble())
+}
+</script>
+
+<style lang="css">
+.wrapper {
+  border: 1px solid black;
+  margin: 10px 0px;
+  padding: 0 10px 10px;
+}
+</style>
