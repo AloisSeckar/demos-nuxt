2d03ca0aa383cc0bcc444971c7ba57e818253bfd
Alois Sečkár <ellrohir@seznam.cz>
2025-03-20 22:03:03 +0100

    build: bump deps for nuxt-content3 demo

--- nuxt-content3/nuxt.config.ts
+++ nuxt-content3/nuxt.config.ts
@@ -1,11 +1,13 @@
 // Nuxt project configuration like modules or runtime variables goes here
 // https://nuxt.com/docs/getting-started/configuration#nuxt-configuration
 export default defineNuxtConfig({
-  compatibilityDate: '2025-02-08',
   modules: [
     '@nuxt/content',
     '@nuxt/eslint',
   ],
+
+  compatibilityDate: '2025-03-20',
+
   eslint: {
     config: {
       stylistic: true,
