09a8d0dbb6e046ef7ed62525ee4097335bb5b44e
Alois Sečkár <ellrohir@seznam.cz>
2025-04-28 23:06:48 +0200

    chore: nuxt config eslint issues

--- nuxt-dx/nuxt.config.ts
+++ nuxt-dx/nuxt.config.ts
@@ -1,12 +1,15 @@
 // Nuxt project configuration like modules or runtime variables goes here
 // https://nuxt.com/docs/getting-started/configuration#nuxt-configuration
 export default defineNuxtConfig({
-
   modules: [
     '@nuxt/eslint',
   ],
+
+  compatibilityDate: '2025-02-08',
+
   // for some reason DevExtreme only works client-side only in production build
-  ssr: false, compatibilityDate: '2025-02-08',
+  ssr: false,
+  
   eslint: {
     config: {
       stylistic: true,
