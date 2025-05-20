5e7429bb4d7cd97ea13c434c8bd382f41ca1549c
Alois sečkár <ellrohir@seznam.cz>
2025-05-20 22:47:17 +0200

    feat: update nuxt-neon module to v0.6.0

--- nuxt-neon-m/components/ExampleSelect.vue
+++ nuxt-neon-m/components/ExampleSelect.vue
@@ -13,6 +13,9 @@
 </template>
 
 <script setup lang="ts">
+import { ref } from 'vue'
+import { useNeon } from '#imports'
+
 const { select } = useNeon()
 
 // custom type to specify awaited results
@@ -27,10 +30,10 @@ async function doSelect() {
   // sql wrapper for `SELECT`
   // returns either an array of expected data type
   // or a string with an error occured (TODO will be changed to NeonError instance)
-  const res = await select<Data>(
-    ['id', 'name', 'value'],
-    'playing_with_neon',
-  )
+  const res = await select<Data>({
+    columns: ['id', 'name', 'value'],
+    from: 'playing_with_neon',
+  })
   if (Array.isArray(res)) {
     result.value = res as Data[]
   }
