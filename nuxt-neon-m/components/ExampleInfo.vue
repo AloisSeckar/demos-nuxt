30abe183371c65cbe510ac1e800bb5b8b47ec2f4
Alois Sečkár <ellrohir@seznam.cz>
2025-06-04 18:11:05 +0200

    feat: update nuxt-neon demo

---
+++ nuxt-neon-m/components/ExampleInfo.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    Sample integration to <a href="https://neon.tech/">Neon database</a> into Nuxt using <a href="https://github.com/AloisSeckar/nuxt-neon/">nuxt-neon module</a> created and maintained by me
+  </div>
+  <img
+    src="https://github.com/AloisSeckar/nuxt-neon/blob/master/playground/public/nuxt-neon.png?raw=true"
+    alt="nuxt-neon module logo"
+    title="nuxt-neon"
+    width="100"
+    height="100"
+  >
+  <div>
+    <strong>NOTE:</strong> own Neon account is necessary to run this demo (see README.md for instructions)
+  </div>
+</template>
---
+++ nuxt-neon-m/components/ExampleStatus.vue
@@ -0,0 +1,11 @@
+<template>
+  <h2>STATUS EXAMPLE</h2>
+  <div>
+    <pre>{{ dbStatus }}</pre>
+  </div>
+</template>
+
+<script setup lang="ts">
+const { neonStatus } = useNeon()
+const dbStatus = await neonStatus(false)
+</script>
