fae72631278750b36a9af2cf83184b90b8926f8a
Alois Sečkár <ellrohir@seznam.cz>
2025-11-15 22:26:33 +0100

    feat: review nuxt-api

---
+++ nuxt-api/app/pages/index.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <h1>Nuxt API</h1>
+    <div>
+      <button @click="handle">
+        Make API call
+      </button>
+    </div>
+    <div>
+      {{ apiResponse }}
+    </div>
+    <h2>Endpoints</h2>
+    <div>
+      <a href="/api/sample">/api/sample</a>
+    </div>
+    <div>
+      <a href="/api/error">/api/error</a>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+const apiResponse = ref('')
+
+const handle = async () => {
+  const data = await $fetch<string>('/api/sample', { method: 'GET' })
+  if (data) {
+    apiResponse.value = data
+  }
+}
+</script>
--- nuxt-api/server/api/sample.get.ts
+++ nuxt-api/server/api/sample.get.ts
@@ -2,6 +2,8 @@
  * http://localhost:3000/api/sample
  *
  * Simple server-side getter method
+ * 
+ * See https://nuxt.com/docs/4.x/directory-structure/server
  */
 
-export default defineEventHandler(() => 'Hello, Nuxt! @ ' + new Date().toISOString())
+export default defineEventHandler(() => 'Hello from Nuxt Server! @ ' + new Date().toISOString())
