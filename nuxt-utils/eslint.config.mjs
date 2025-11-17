3a775f65f3919c95ac58abc6643c6c6e8a577043
Alois Sečkár <ellrohir@seznam.cz>
2025-11-17 22:13:20 +0100

    refactor: fix eslint issues

--- nuxt-utils/eslint.config.mjs
+++ nuxt-utils/eslint.config.mjs
@@ -4,7 +4,7 @@ import withNuxt from './.nuxt/eslint.config.mjs'
 // as suggested here: https://github.com/nuxt/eslint/discussions/413
 
 export default withNuxt([
-  
+
   // `rules` section can follow, where you can change default eslint behaviour if needed
   // you can adjust or even turn off some rules if you cannot or don't want to satisfy them
 
