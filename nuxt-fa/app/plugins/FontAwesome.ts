8716d71e60a4448c7e8c1487ba783fac88b60845
Alois Sečkár <ellrohir@seznam.cz>
2026-01-11 11:25:59 +0100

    docs: add comments into fa plugin definition

--- nuxt-fa/app/plugins/FontAwesome.ts
+++ nuxt-fa/app/plugins/FontAwesome.ts
@@ -1,11 +1,16 @@
+import {
+  faEnvelope, faFaceSmile, faHouse,
+} from '@fortawesome/free-solid-svg-icons'
 import { library } from '@fortawesome/fontawesome-svg-core'
-import { faEnvelope, faFaceSmile, faHouse } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
+// those icons will be available as values of "icon" attributes
 library.add(faEnvelope)
 library.add(faFaceSmile)
 library.add(faHouse)
 
+// "fa" component will be available globally
+// usage: <fa icon="..." />
 export default defineNuxtPlugin((nuxtApp) => {
   nuxtApp.vueApp.component('fa', FontAwesomeIcon)
 })
