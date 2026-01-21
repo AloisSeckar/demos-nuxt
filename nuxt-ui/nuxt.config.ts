964d522ffc4071a329b09e721ba90fd39640bbfd
Alois Sečkár <ellrohir@seznam.cz>
2026-01-21 19:57:36 +0100

    feat: review nuxt-ui

--- nuxt-ui/nuxt.config.ts
+++ nuxt-ui/nuxt.config.ts
@@ -1,11 +1,17 @@
 // Nuxt project configuration like modules or runtime variables goes here
 // https://nuxt.com/docs/getting-started/configuration#nuxt-configuration
 export default defineNuxtConfig({
-  compatibilityDate: '2025-02-08',
   modules: [
     '@nuxt/eslint',
     '@nuxt/ui',
   ],
+
+  // load Tailwind CSS and Nuxt UI styles
+  css: ['~/assets/main.css'],
+
+  compatibilityDate: '2026-01-21',
+
+  // simple eslint config
   eslint: {
     config: {
       stylistic: true,
