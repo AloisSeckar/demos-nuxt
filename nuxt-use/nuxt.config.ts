cba04c633a161359c1c1825809987fd59331b7f6
Alois Sečkár <ellrohir@seznam.cz>
2026-01-22 20:39:52 +0100

    feat: review nuxt-use

--- nuxt-use/nuxt.config.ts
+++ nuxt-use/nuxt.config.ts
@@ -1,19 +1,22 @@
 // Nuxt project configuration like modules or runtime variables goes here
 // https://nuxt.com/docs/getting-started/configuration#nuxt-configuration
 export default defineNuxtConfig({
-  compatibilityDate: '2025-04-28',
   modules: [
     '@nuxt/eslint',
     '@vueuse/nuxt',
   ],
-  eslint: {
-    config: {
-      stylistic: true,
-    },
-  },
+
   routeRules: {
     // there would be an issue with `useMouse()` and `v-bind` in CSS
     // if we try to render this page on the server...
     '/mouse': { ssr: false },
+  },
+
+  compatibilityDate: '2026-01-22',
+
+  eslint: {
+    config: {
+      stylistic: true,
+    },
   },
 })
