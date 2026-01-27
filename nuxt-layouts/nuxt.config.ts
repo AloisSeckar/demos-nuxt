5b791be9fa6c8e7637d14848e060325c1743f832
Alois Sečkár <ellrohir@seznam.cz>
2026-01-27 20:57:50 +0100

    feat: review nuxt-layouts

--- nuxt-layouts/nuxt.config.ts
+++ nuxt-layouts/nuxt.config.ts
@@ -1,10 +1,12 @@
 // Nuxt project configuration like modules or runtime variables goes here
 // https://nuxt.com/docs/getting-started/configuration#nuxt-configuration
 export default defineNuxtConfig({
-  compatibilityDate: '2025-02-08',
   modules: [
     '@nuxt/eslint',
   ],
+
+  compatibilityDate: '2026-01-27',
+
   eslint: {
     config: {
       stylistic: true,
