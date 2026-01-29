f7c225aee8ab3c29efd48a266c328871f3960331
Alois Sečkár <ellrohir@seznam.cz>
2026-01-29 21:20:31 +0100

    feat: review nuxt-formkit

--- nuxt-formkit/nuxt.config.ts
+++ nuxt-formkit/nuxt.config.ts
@@ -1,12 +1,15 @@
 // Nuxt project configuration like modules or runtime variables goes here
 // https://nuxt.com/docs/getting-started/configuration#nuxt-configuration
 export default defineNuxtConfig({
-  compatibilityDate: '2025-02-08',
-  css: ['@formkit/themes/genesis'],
   modules: [
     '@formkit/nuxt',
     '@nuxt/eslint',
   ],
+
+  css: ['@formkit/themes/genesis'],
+
+  compatibilityDate: '2025-02-08',
+
   eslint: {
     config: {
       stylistic: true,
